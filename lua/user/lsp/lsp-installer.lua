46452f6ee72c400d88abbbc3a395ea42e3ab988d
Alpha <alpha.vylly12@gmail.com>
2022-08-02 01:39:08 -0300

    feat(WIP): removed some configs about rust and add emoji cmp

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -5,42 +5,38 @@ end
 
 local servers = {
   -- Search for more in https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-  "sumneko_lua",            -- Lua
-  "asm_lsp",                -- Assembly/Nasm
-  "clangd",                 -- C/C++
-  -- "cmake",                  -- CMake
-  "cssls",                  -- CSS
-  "html",                   -- HTML
-  -- "erlangls",               -- Erlang
-  "eslint",                 -- Eslint
-  "tsserver",               -- JavaScript/TypeScript
-  "pyright",                -- Python
-  "java_language_server",   -- Java
+  "sumneko_lua", -- Lua
+  "asm_lsp", -- Assembly/Nasm
+  "clangd", -- C/C++
+  "cssls", -- CSS
+  "html", -- HTML
+  "eslint", -- Eslint
+  "tsserver", -- JavaScript/TypeScript
+  "pyright", -- Python
+  "java_language_server", -- Java
   "kotlin_language_server", -- Kotlin
-  "graphql",                -- GraphQL
-  "elixirls",               -- Elixir
-  -- "hls",                    -- Haskell
-  -- "rnix",                   -- Nix
-  "bashls",                 -- Bash
-  "jsonls",                 -- Json
-  "yamlls",                 -- Yaml
-  "rust_analyzer",          -- Rust
-  "taplo",                  -- TOML
-  "emmet_ls",               -- Emmet
-  "dockerls",               -- Docker
-  "denols",                 -- Deno
-  "solargraph",             -- Ruby
+  "graphql", -- GraphQL
+  "elixirls", -- Elixir
+  "hls", -- Haskell
+  "bashls", -- Bash
+  "jsonls", -- Json
+  "yamlls", -- Yaml
+  "rust_analyzer", -- Rust
+  "emmet_ls", -- Emmet
+  "dockerls", -- Docker
+  "denols", -- Deno
+  "solargraph", -- Ruby
 }
 
 lsp_installer.setup({
-    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
+  automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+  ui = {
+    icons = {
+      server_installed = "✓",
+      server_pending = "➜",
+      server_uninstalled = "✗"
     }
+  }
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
@@ -62,28 +58,6 @@ for _, server in pairs(servers) do
   end
 
   if server == "rust_analyzer" then
-    local keymap = vim.keymap.set
-    local key_opts = { silent = true }
-
-    keymap("n", "<leader>rh", "<cmd>RustSetInlayHints<Cr>", key_opts)
-    keymap("n", "<leader>rhd", "<cmd>RustDisableInlayHints<Cr>", key_opts)
-    keymap("n", "<leader>th", "<cmd>RustToggleInlayHints<Cr>", key_opts)
-    keymap("n", "<leader>rr", "<cmd>RustRunnables<Cr>", key_opts)
-    keymap("n", "<leader>rem", "<cmd>RustExpandMacro<Cr>", key_opts)
-    keymap("n", "<leader>roc", "<cmd>RustOpenCargo<Cr>", key_opts)
-    keymap("n", "<leader>rpm", "<cmd>RustParentModule<Cr>", key_opts)
-    keymap("n", "<leader>rjl", "<cmd>RustJoinLines<Cr>", key_opts)
-    keymap("n", "<leader>rha", "<cmd>RustHoverActions<Cr>", key_opts)
-    keymap("n", "<leader>rhr", "<cmd>RustHoverRange<Cr>", key_opts)
-    keymap("n", "<leader>rmd", "<cmd>RustMoveItemDown<Cr>", key_opts)
-    keymap("n", "<leader>rmu", "<cmd>RustMoveItemUp<Cr>", key_opts)
-    keymap("n", "<leader>rsb", "<cmd>RustStartStandaloneServerForBuffer<Cr>", key_opts)
-    keymap("n", "<leader>rd", "<cmd>RustDebuggables<Cr>", key_opts)
-    keymap("n", "<leader>rv", "<cmd>RustViewCrateGraph<Cr>", key_opts)
-    keymap("n", "<leader>rw", "<cmd>RustReloadWorkspace<Cr>", key_opts)
-    keymap("n", "<leader>rss", "<cmd>RustSSR<Cr>", key_opts)
-    keymap("n", "<leader>rxd", "<cmd>RustOpenExternalDocs<Cr>", key_opts)
-
     require("rust-tools").setup {
       tools = {
         on_initialized = function()
