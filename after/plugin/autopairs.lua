35c66f7d6a38910cf8ed6cc41084b8c2d1bb41a4
AlphaLawless <alpha.vylly12@gmail.com>
2023-08-04 17:22:38 -0300

    update: auto-pairs complete

--- after/plugin/autopairs.lua
+++ after/plugin/autopairs.lua
@@ -6,5 +6,16 @@ end
 npairs.setup({
   map_cr = true,
   enable_check_bracket_line = false,
-  disable_filetype = { "TelescopePrompt" , "vim" },
+  disable_filetype = { "TelescopePrompt", "vim" },
+  fast_wrap = {
+    map = '<M-e>',
+    chars = { '{', '[', '(', '"', "'", '`' },
+    pattern = [=[[%'%"%>%]%)%}%,]]=],
+    end_key = '$',
+    keys = 'qwertyuiopzxcvbnmasdfghjkl',
+    check_comma = true,
+    manual_position = true,
+    highlight = 'Search',
+    highlight_grey = 'Comment'
+  },
 })
